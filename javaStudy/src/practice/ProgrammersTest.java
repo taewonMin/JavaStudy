ac7e1805eb101bc93e97275b2a7be00f10592b87
taewonMin <mtw9411@naver.com>
2020-12-16 21:01:57 +0900

    programmers test2

--- javaStudy/src/practice/ProgrammersTest.java
+++ javaStudy/src/practice/ProgrammersTest.java
@@ -1,10 +1,6 @@
 package practice;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.List;
-import java.util.Stack;
 
 public class ProgrammersTest {
 	public static void main(String[] args) {
